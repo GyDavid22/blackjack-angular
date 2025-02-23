1fa18d549b91d7fa8d1ebf017129615fb6b012f3
David Gyenes <gyenesdavid@gmail.com>
2025-02-23 10:19:17 +0100

    Added animate.css

--- src/app/srv/Helper.ts
+++ src/app/srv/Helper.ts
@@ -1,3 +1,20 @@
 export async function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function addAnimation(element: Element, className: string, after?: () => void) {
+    element.classList.add('animate__animated', className);
+    const fn = () => {
+        element.removeEventListener('animationend', fn);
+        element.classList.remove('animate__animated', className);
+        if (after) {
+            after();
+        }
+    };
+    element.addEventListener('animationend', fn);
+}
+
+export const Animations = {
+    'fade in': 'animate__fadeIn',
+    'fade out': 'animate__fadeOut'
+};
